5f5490945ec61c29645f643955b58f90efa8d8c9
kailinwei <kailinwei95@gmail.com>
2021-04-06 11:54:06 -0700

    adding php

--- review/database.php/config.php
+++ review/database.php/config.php
@@ -2,6 +2,6 @@
 // Define the constants for host, dbname, username and password
 define('DBHOST', 'localhost');
 define('DBNAME', 'a8');
-define('DBUSER', 'public');
-define('DBPASS', 'password');
+define('DBUSER', 'root');
+define('DBPASS', '');
  ?>
--- review/templates/index.php
+++ review/templates/index.php
@@ -14,6 +14,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="menu.js" charset="utf-8"></script>
+    <script language="javascript" type="text/javascript" src="jquery.js"></script>
   </head>
 
   <body>
@@ -109,5 +110,35 @@
         </div>
       </div>
     </footer>
+
+
+
+    <script id="source" language="javascript" type="text/javascript">
+
+  $(function () 
+  {
+    //-----------------------------------------------------------------------
+    // 2) Send a http request with AJAX http://api.jquery.com/jQuery.ajax/
+    //-----------------------------------------------------------------------
+    $.ajax({                                      
+      url: 'api.php',                  //the script to call to get data          
+      data: "",                        //you can insert url argumnets here to pass to api.php
+                                       //for example "id=5&parent=6"
+      dataType: 'json',                //data format      
+      success: function(data)          //on recieve of reply
+      {
+        var id = data[0];              //get id
+        var vname = data[1];           //get name
+        //--------------------------------------------------------------------
+        // 3) Update html content
+        //--------------------------------------------------------------------
+        $('#output').html("<b>id: </b>"+id+"<b> name: </b>"+vname); //Set output element html
+        //recommend reading up on jquery selectors they are awesome 
+        // http://api.jquery.com/category/selectors/
+      } 
+    });
+  }); 
+
+  </script>
   </body>
 </html>
